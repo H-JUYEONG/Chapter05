07a0afef836b135a361530fce1396a6713156465
H-JUYEONG <dud9902@gmail.com>
2024-07-15 14:57:56 +0900

    2024.07.15 FileReader MS949

--- src/com/javaex/ex03/Ex01.java
+++ src/com/javaex/ex03/Ex01.java
@@ -1,44 +1,30 @@
 package com.javaex.ex03;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.Reader;
 
 public class Ex01 {
 
 	public static void main(String[] args) throws IOException {
 
-		// 읽기 스트림
-		InputStream in = new FileInputStream("C:\\javaStudy\\MS949.txt");
-		InputStreamReader isr = new InputStreamReader(in, "MS949");
-		BufferedReader br = new BufferedReader(isr);
-
-		// 쓰기 스트림
-		OutputStream out = new FileOutputStream("C:\\javaStudy\\MS949-copy.txt");
-		OutputStreamWriter osw = new OutputStreamWriter(out, "MS949");
-		BufferedWriter bw = new BufferedWriter(osw);
+		Reader fr = new FileReader("C:\\javaStudy\\MS949.txt");
+		BufferedReader br = new BufferedReader(fr);
 
 		while (true) {
-			String str = br.readLine();
+			String str = br.readLine(); // 파일의 내용을 1줄씩 읽어온다.
 			if (str == null) {
 				System.out.println("파일 읽기 끝");
 				break;
 			}
-
-			bw.write(str);
-			bw.newLine();
-			// System.out.println(str);
+			
+			System.out.println(str);
 		}
-		
-		bw.close();
+
 		br.close();
 		System.out.println("프로그램 종료");
+
 	}
 
 }
