b168b05cc46c3a94d750b6040a3856ce9cb2c263
H-JUYEONG <dud9902@gmail.com>
2024-07-15 14:40:55 +0900

    2024.07.15 I/O StreamReader

--- src/com/javaex/ex03/Ex01.java
+++ src/com/javaex/ex03/Ex01.java
@@ -1,18 +1,28 @@
 package com.javaex.ex03;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class Ex01 {
 
 	public static void main(String[] args) throws IOException {
 
+		// 읽기 스트림
 		InputStream in = new FileInputStream("C:\\javaStudy\\MS949.txt");
 		InputStreamReader isr = new InputStreamReader(in, "MS949");
 		BufferedReader br = new BufferedReader(isr);
+
+		// 쓰기 스트림
+		OutputStream out = new FileOutputStream("C:\\javaStudy\\MS949-copy.txt");
+		OutputStreamWriter osw = new OutputStreamWriter(out, "MS949");
+		BufferedWriter bw = new BufferedWriter(osw);
 
 		while (true) {
 			String str = br.readLine();
@@ -20,11 +30,14 @@ public class Ex01 {
 				System.out.println("파일 읽기 끝");
 				break;
 			}
-			System.out.println(str);
 
+			bw.write(str);
+			bw.newLine();
+			// System.out.println(str);
 		}
-
-		isr.close();
+		
+		bw.close();
+		br.close();
 		System.out.println("프로그램 종료");
 	}
 
