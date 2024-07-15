82c59686f79ca152b3132660b34fe514fdc5ad21
H-JUYEONG <dud9902@gmail.com>
2024-07-15 16:22:16 +0900

    2024.07.15 InputStreamReader

---
+++ src/com/javaex/ex03/Ex03.java
@@ -0,0 +1,51 @@
+package com.javaex.ex03;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex03 {
+
+	public static void main(String[] args) throws IOException {
+
+		List<Person> person = new ArrayList<Person>();
+
+		// 읽기 스트림
+		InputStream in = new FileInputStream("C:\\javaStudy\\MS949.txt");
+		InputStreamReader isr = new InputStreamReader(in, "MS949");
+		BufferedReader br = new BufferedReader(isr);
+
+		// 한줄씩 읽기 반복
+		while (true) {
+			// 파일을 한줄씩 읽는다.
+			String str = br.readLine();
+
+			// 데이터 없으면 끝
+			if (str == null) {
+				break;
+			}
+
+			// 데이터가 있으면
+			// 데이터를 3기준으로 나눠주고
+			String[] file = str.split("#");
+
+			String name = file[0];
+			String hp = file[1];
+			String company = file[2];
+
+			Person p = new Person(name, hp, company);
+			person.add(p);
+		}
+
+		for (int i = 0; i < person.size(); i++) {
+			person.get(i).showInfo();
+		}
+
+		br.close();
+	}
+
+}
---
+++ src/com/javaex/ex03/Person.java
@@ -0,0 +1,50 @@
+package com.javaex.ex03;
+
+public class Person {
+
+	private String name;
+	private String hp;
+	private String company;
+
+	public Person() {
+
+	}
+
+	public Person(String name, String hp, String company) {
+		this.name = name;
+		this.hp = hp;
+		this.company = company;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public void showInfo() {
+		System.out.println("----------------------");
+		System.out.println("이름: " + name);
+		System.out.println("전화: " + hp);
+		System.out.println("회사: " + company);
+		System.out.println("----------------------");
+	}
+}
