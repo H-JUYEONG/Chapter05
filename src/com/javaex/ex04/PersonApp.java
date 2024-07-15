3036e839116b634fc34874852d800bd78791b506
H-JUYEONG <dud9902@gmail.com>
2024-07-15 16:32:55 +0900

    2024.07.15 InputStreamReader

--- src/com/javaex/ex04/PersonApp.java
+++ src/com/javaex/ex04/PersonApp.java
@@ -1,4 +1,4 @@
-package com.javaex.ex03;
+package com.javaex.ex04;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Ex03 {
+public class PersonApp {
 
 	public static void main(String[] args) throws IOException {
 
